2e473650bac0d8eab3f75c0aa455f00a11a6e74a
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-07-25 22:25:52 -0400

    vinculados css y js

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require 'res/php/app_top.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="res/css/semantic.min.css">
+  </head>
+  <body>
+  <script src="res/js/semantic.min.js"></script>
+  <script src="res/js/jquery.min.js"></script>
+  </body>
+</html>
