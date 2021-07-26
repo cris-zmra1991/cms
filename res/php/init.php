59a8f8588d77853978b3d31f1f682be3e2a6fadf
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-07-25 22:06:39 -0400

    conexion a BD creada

---
+++ res/php/init.php
@@ -0,0 +1,16 @@
+<?php
+require 'medoo.php';
+use Medoo\Medoo;
+try {
+  $database = new Medoo([
+  	// [required]
+  	'type' => 'mysql',
+  	'host' => 'localhost',
+  	'database' => 'cms',
+  	'username' => 'root',
+  	'password' => '',
+   ]);
+} catch (PDOException $e) {
+  echo "No se pudo conectar a la Base de Datos";
+}
+?>
